d501d60dd973843e1d93a35689f6cbeb7795afb6
Rüdiger Block <rblock@wechselwesen.com>
2014-06-11 16:47:23 +0200

    added new entry class; removed stupid <<,>> buttons

---
+++ Gui/entry.rb
@@ -0,0 +1,20 @@
+module Gui
+  class Entry < Gtk::Entry
+    def initialize 
+      super   
+      self.signal_connect "scroll-event" do |w,e|
+        case e.direction
+          when Gdk::EventScroll::Direction::UP
+            adder = "+0.25"
+          when Gdk::EventScroll::Direction::DOWN
+           adder = "-0.25"
+        end
+      result = (w.text.to_f+adder.to_f)
+      if result >= 0
+        w.text=result.to_s
+      end
+       w.parent.parent.parent.parent.update_data
+      end
+    end
+  end
+end
